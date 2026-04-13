c49aac23cd0d86e19abe18409446789c5701bbc7
komarinet <112837242+komarinet@users.noreply.github.com>
2026-04-13 17:22:17 +0900

    Create scenario_kagami.js

---
+++ game002/js/scenario_kagami.js
@@ -0,0 +1,11 @@
+// 各務 栞 ルートのシナリオデータ
+scenarios.kagami = {
+    'opening': [],
+    'kagami_arrival': [],
+    1: { adv: [], pre_stg: [], post_stg: [] },
+    2: { adv: [], pre_stg: [], post_stg: [] },
+    3: { adv: [], pre_stg: [], post_stg: [] },
+    4: { adv: [], pre_stg: [], post_stg: [] },
+    5: { adv: [], pre_stg: [], post_stg: [] },
+    6: { adv: [], pre_stg: [], post_stg: [] }
+};
