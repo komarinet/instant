7617ecd92923af7c7fa24c1c926bc8189e7a211d
komarinet <112837242+komarinet@users.noreply.github.com>
2026-04-13 17:22:37 +0900

    Create scenario_godai.js

---
+++ game002/js/scenario_godai.js
@@ -0,0 +1,11 @@
+// G・O・D・A・I ルートのシナリオデータ
+scenarios.godai = {
+    'opening': [],
+    'kagami_arrival': [],
+    1: { adv: [], pre_stg: [], post_stg: [] },
+    2: { adv: [], pre_stg: [], post_stg: [] },
+    3: { adv: [], pre_stg: [], post_stg: [] },
+    4: { adv: [], pre_stg: [], post_stg: [] },
+    5: { adv: [], pre_stg: [], post_stg: [] },
+    6: { adv: [], pre_stg: [], post_stg: [] }
+};
