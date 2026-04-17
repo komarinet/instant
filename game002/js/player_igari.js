38916ed88906c63690f52e4876aefb9b6e3fec4e
komarinet <112837242+komarinet@users.noreply.github.com>
2026-04-18 01:28:27 +0900

    Create player_igari.js

---
+++ game002/js/player_igari.js
@@ -0,0 +1,181 @@
+window.PlayerControllers = window.PlayerControllers || {};
+
+// 猪狩専用の奥義レーザーエフェクト
+class BombLaserIgari {
+    constructor(canvasWidth, canvasHeight) {
+        this.sW = canvasWidth; this.sH = canvasHeight;
+        this.alive = true; this.state = 'WINDUP'; this.timer = 0;
+        this.windupHeight = 0; this.windupDuration = 30; 
+        this.beamDuration = 60; this.beamAlpha = 1.0;
+    }
+    update() {
+        this.timer++;
+        if (this.state === 'WINDUP') {
+            this.windupHeight = this.sH * (this.timer / this.windupDuration); 
+            if (this.timer >= this.windupDuration) { this.state = 'BEAM'; this.timer = 0; }
+        } else if (this.state === 'BEAM') {
+            this.beamAlpha = Math.max(0, 1.0 - (this.timer / this.beamDuration));
+            if (this.timer >= this.beamDuration) { this.state = 'DONE'; this.alive = false; }
+        }
+    }
+    draw(ctx) {
+        ctx.save();
+        if (this.state === 'WINDUP' && this.windupHeight > 0) {
+            const targetY = Math.max(0, this.sH - this.windupHeight);
+            if (targetY < this.sH) {
+                const gradient = ctx.createLinearGradient(0, this.sH, 0, targetY);
+                gradient.addColorStop(0, 'rgba(255, 255, 255, 0)'); gradient.addColorStop(0.5, 'rgba(200, 0, 255, 0.8)'); gradient.addColorStop(1, 'rgba(255, 255, 255, 1)');
+                ctx.fillStyle = gradient; ctx.fillRect(0, targetY, this.sW, this.windupHeight);
+                ctx.strokeStyle = '#fff'; ctx.lineWidth = 10; ctx.shadowColor = 'magenta'; ctx.shadowBlur = 20;
+                ctx.beginPath(); ctx.moveTo(0, targetY); ctx.lineTo(this.sW, targetY); ctx.stroke();
+            }
+        } else if (this.state === 'BEAM') {
+            ctx.globalAlpha = this.beamAlpha;
+            ctx.fillStyle = '#fff'; ctx.fillRect(0, 0, this.sW, this.sH);
+            ctx.fillStyle = 'rgba(100, 0, 255, 0.5)'; ctx.shadowColor = 'magenta'; ctx.shadowBlur = 50; ctx.fillRect(0, 0, this.sW, this.sH);
+        }
+        ctx.restore();
+    }
+}
+
+// 猪狩の行動コントローラー
+window.PlayerControllers['igari'] = {
+    draw: function(player, ctx, advManager) {
+        let img = (advManager && advManager.assets) ? advManager.assets['igari_jiki.png'] : null;
+        if (img && img.naturalHeight > 0) {
+            const drawWidth = 60;
+            const drawHeight = drawWidth * (img.naturalHeight / img.naturalWidth);
+            ctx.drawImage(img, player.x - drawWidth / 2, player.y - drawHeight / 2, drawWidth, drawHeight);
+            ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+            ctx.beginPath(); ctx.arc(player.x, player.y, 4, 0, Math.PI*2); ctx.fill();
+        } else {
+            ctx.fillStyle = player.color; ctx.beginPath();
+            ctx.moveTo(player.x, player.y - player.size); ctx.lineTo(player.x - player.size, player.y + player.size); ctx.lineTo(player.x + player.size, player.y + player.size);
+            ctx.closePath(); ctx.fill();
+        }
+    },
+
+    shoot: function(player) {
+        const bS = 10;
+        // igari専用のレーザーとして生成
+        const pL = player.powerLevel;
+        if (pL === 0) { player.bullets.push(this.createLaser(player.x, player.y - player.size, 0, -bS, player.color)); }
+        else if (pL === 1) { player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, 0, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 0, -bS, player.color)); }
+        else if (pL === 2) {
+            player.bullets.push(this.createLaser(player.x, player.y - player.size, 0, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -1, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 1, -bS, player.color));
+        }
+        else if (pL === 3) {
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -0.5, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 0.5, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -1.5, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 1.5, -bS, player.color));
+        }
+        else if (pL === 4) {
+            player.bullets.push(this.createLaser(player.x, player.y - player.size, 0, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -1, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 1, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -2, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 2, -bS, player.color));
+        }
+        else if (pL === 5) {
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -0.5, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 0.5, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -1.5, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 1.5, -bS, player.color));
+            player.bullets.push(this.createLaser(player.x - 5, player.y - player.size, -2.5, -bS, player.color)); player.bullets.push(this.createLaser(player.x + 5, player.y - player.size, 2.5, -bS, player.color));
+        }
+        else if (pL === 6) { for (let i = -3; i <= 3; i++) player.bullets.push(this.createLaser(player.x, player.y - player.size, i * 1.0, -bS, player.color)); }
+        else if (pL === 7) { for (let i = -3; i <= 4; i++) player.bullets.push(this.createLaser(player.x - 2.5, player.y - player.size, (i - 0.5) * 1.0, -bS, player.color)); }
+        else if (pL >= 8) { for (let i = -4; i <= 4; i++) player.bullets.push(this.createLaser(player.x, player.y - player.size, i * 1.0, -bS, player.color)); }
+    },
+    
+    // レーザーを描画するように改造された特別なBulletを生成する
+    createLaser: function(x, y, vx, vy, color) {
+        let b = new Bullet(x, y, vx, vy, color, null, 'igari');
+        b.draw = function(ctx) {
+            ctx.save(); ctx.translate(this.x, this.y);
+            ctx.rotate(Math.atan2(this.vy, this.vx));
+            const length = this.size * 2.5; 
+            ctx.shadowColor = this.color; ctx.shadowBlur = this.size * 2.5; 
+            ctx.strokeStyle = this.color; ctx.lineWidth = this.size; ctx.lineCap = 'round'; 
+            ctx.beginPath(); ctx.moveTo(-length, 0); ctx.lineTo(length, 0); ctx.stroke();
+            ctx.shadowBlur = 0; ctx.strokeStyle = '#ffffff'; ctx.lineWidth = this.size * 0.4;
+            ctx.beginPath(); ctx.moveTo(-length * 0.8, 0); ctx.lineTo(length * 0.8, 0); ctx.stroke();
+            ctx.restore();
+        };
+        return b;
+    },
+
+    triggerBomb: function(player, stg) {
+        if (player.bombs <= 0 || stg.bombState !== 'READY') return;
+        player.bombs--; 
+        stg.bombState = 'ANIM_IN';
+        stg.bombTimer = 0;
+        stg.isTimeStopped = true; 
+        
+        const canvas = document.getElementById('gameCanvas'), dpr = window.devicePixelRatio || 1;
+        stg.bombData = {
+            x: canvas.width / dpr,
+            y: (canvas.height / dpr) * 0.7,
+            img: (stg.advManager && stg.advManager.assets) ? stg.advManager.assets['igaribomb.png'] : null,
+            laser: null
+        };
+        stg.enemyBullets = [];
+        stg.explosions.push(new Explosion(player.x, player.y, player.size * 4, stg.advManager));
+    },
+
+    updateBomb: function(player, stg, sW, sH) {
+        stg.bombTimer++;
+        let bd = stg.bombData;
+        
+        if (stg.bombState === 'ANIM_IN') {
+            if (stg.bombTimer < 30) {
+                const imgH = sH / 3;
+                let imgW = 200; 
+                if (bd.img && bd.img.naturalHeight > 0) { imgW = imgH * (bd.img.naturalWidth / bd.img.naturalHeight); }
+                const targetX = sW - (imgW * 0.5 + 20); 
+                bd.x -= (bd.x - targetX) * 0.2; 
+            }
+            else if (stg.bombTimer >= 60 && stg.bombTimer < 80) { bd.x -= 20; }
+            else if (stg.bombTimer >= 80) {
+                if (!bd.laser) {
+                    bd.laser = new BombLaserIgari(sW, sH);
+                    stg.flashTimer = 10; stg.shakeTimer = 30;
+                }
+                bd.laser.update();
+                
+                if (bd.laser.state === 'BEAM') {
+                    stg.enemies.forEach(e => {
+                        if (!e.isDying && e.type !== 'typeboss') {
+                            e.alive = false; stg.explosions.push(new Explosion(e.x, e.y, e.size * 2, stg.advManager));
+                        } else if (e.type === 'typeboss') {
+                            e.hp -= 20; 
+                            if (e.hp <= 0 && !e.isDying) { e.isDying = true; e.deathTimer = 0; stg.enemyBullets = []; }
+                        }
+                    });
+                    stg.flashTimer = 20; stg.shakeTimer = 60; stg.bombState = 'BEAM'; 
+                }
+            }
+        }
+        else if (stg.bombState === 'BEAM') {
+            bd.laser.update();
+            if (bd.laser.state === 'DONE') {
+                stg.isTimeStopped = false; 
+                stg.bombState = 'READY'; // もう一度撃てるようにリセット
+                stg.bombData = null;
+            }
+        }
+    },
+
+    drawBomb: function(player, stg, ctx, sW, sH, shakeX, shakeY) {
+        let bd = stg.bombData;
+        if (!bd) return;
+
+        if (stg.bombState === 'ANIM_IN' && bd.img && stg.bombTimer < 80) {
+            if (bd.img.naturalHeight > 0) {
+                ctx.save(); ctx.translate(-shakeX, -shakeY); 
+                const imgH = sH / 3;
+                const imgW = imgH * (bd.img.naturalWidth / bd.img.naturalHeight);
+                ctx.drawImage(bd.img, bd.x - imgW * 0.5, bd.y - imgH * 0.5, imgW, imgH);
+                ctx.restore();
+            }
+        }
+        if (bd.laser) bd.laser.draw(ctx); 
+    }
+};
