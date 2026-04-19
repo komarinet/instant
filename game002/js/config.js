48c2ae53277b5e02dfac8a8d9e98dec6049d3d61
komarinet <112837242+komarinet@users.noreply.github.com>
2026-04-19 21:56:11 +0900

    Create config.js

---
+++ game002/js/config.js
@@ -0,0 +1,27 @@
+// js/config.js
+export const VER_MAIN = "0.7.0"; // モジュール化移行版
+
+export const imagesToPreload = [
+    'airport.png', 'igari02.png', 'hiragi01.png', 'kagami.png', 'room.png', 'igni.png', 'breakufo.png',
+    'breakplane.png', 
+    'typea.png', 'typeb.png', 'typec.png', 'typeboss.png',
+    '2typea.png', '2typeb.png', '2typec.png', '2typeboss.png', 
+    'darkcandle.png',
+    'hospital.png', 'mountain.png','sanrin.png', 'yakerin.png', 
+    'shiina.png', 'urashiina.png',
+    'baku01.png',
+    'igari_jiki.png',
+    'igaribomb.png',
+    // --- ステージ3追加分 ---
+    'shiinaboss.png',
+    'shiki.png',
+    'sans.png'
+];
+
+export const imagesToPreload3D = [
+    { key: 'sideatlas', src: 'build_side.png' }, 
+    { key: 'topatlas', src: 'build_top.png' },
+    { key: 'ground', src: 'ground01.png' },
+    { key: 'ground2', src: 'ground02.png' }, 
+    { key: 'candle', src: 'candle.png' } 
+];
