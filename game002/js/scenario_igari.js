e05d86241a37b6fd4c4a78318626b695a8deae26
komarinet <112837242+komarinet@users.noreply.github.com>
2026-04-27 14:01:15 +0900

    Update scenario_igari.js

--- game002/js/scenario_igari.js
+++ game002/js/scenario_igari.js
@@ -30,7 +30,7 @@ scenarios.igari = {
         { bg: 'room.png', place: 'Room', time: '2025.04', se: 'alarm.mp3', text: '' },
         { bg: 'room.png', place: 'Room', time: '2025.04', character: 'igari02.png', spriteIndex: 9, speaker: '猪狩', text: 'はっ・・・夢？', bgm: 'relax' },
         { bg: 'room.png', place: 'Room', time: '2025.04', character: 'igari02.png', spriteIndex: 8, speaker: '猪狩', text: 'てか、俺なんで家にいるんだよ！　いま何時だ、仕事が・・・' },
-        { bg: 'room.png', place: 'Room', time: '2025.04', item: 'phone.png', text: '' },
+        { bg: 'room.png', place: 'Room', time: '2025.04', item: 'phone3.png', text: '' },
         { bg: 'room.png', place: 'Room', time: '2025.04', character: 'igari02.png', spriteIndex: 11, speaker: '猪狩', text: '・・・2025年4月2日？' },
         { bg: 'room.png', place: 'Room', time: '2025.04',item: 'phone2.png', se: 'vibration.mp3', text: '' },
         { bg: 'room.png', place: 'Room', time: '2025.04', character: 'igari02.png', spriteIndex: 0, speaker: '猪狩', text: 'はい、猪狩ですが' },
