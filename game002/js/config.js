30f46ef399788435c195d73c8374f55775aa4c95
komarinet <112837242+komarinet@users.noreply.github.com>
2026-05-12 10:56:44 +0900

    Update config.js

--- game002/js/config.js
+++ game002/js/config.js
@@ -20,7 +20,7 @@ export const imagesToPreload = [
     // --- 新規追加分 ---
     'trench_floor.png','trench_wall.png','​core_bg.png','core_reactor.png','gtyped.png','gtypee.png'
     ,'cap01.png','uramoon.png','base.png','inbase.png','brokenbase.png', 'airport02.png', 'jikishi.png'
-    ,'jikishi.png'
+    ,'jikishi.png','chain.png'
 ];
 
 export const imagesToPreload3D = [
