abb27020f7913b0441d81679277a5b80b4d4d639
komarinet <112837242+komarinet@users.noreply.github.com>
2026-05-27 12:18:14 +0900

    Update config.js

--- game002/js/config.js
+++ game002/js/config.js
@@ -1,39 +1,39 @@
 export const VER_CONFIG = "0.1.2"; // 3D用の月テクスチャ追加
-
+ 
 export const imagesToPreload = [
-    'airport.png', 'igari02.png', 'hiragi01.png', 'kagami.png', 'room.png', 'igni.png', 'breakufo.png',
-    'breakplane.png', 'citysky.png','city.png','citypanic.png','breakufo2.png','calendar.png',
-    'typea.png', 'typeb.png', 'typec.png', 'typeboss.png','shiinabomb.png','meeting.png',
-    '2typea.png', '2typeb.png', '2typec.png', '2typeboss.png', 'cityboss.png',
-    'darkcandle.png','hospital.png', 'mountain.png','sanrin.png', 'yakerin.png', 
-    'shiina.png', 'urashiina.png','baku01.png','igari_jiki.png','igaribomb.png',
+    'airport.webp’, 'igari02.webp’, 'hiragi01.webp’, 'kagami.webp’, 'room.webp’, 'igni.webp’, 'breakufo.webp’,
+    'breakplane.webp’, 'citysky.webp’,'city.webp’,'citypanic.webp’,'breakufo2.webp’,'calendar.webp’,
+    'typea.webp’, 'typeb.webp’, 'typec.webp’, 'typeboss.webp’,'shiinabomb.webp’,'meeting.webp’,
+    '2typea.webp’, '2typeb.webp’, '2typec.webp’, '2typeboss.webp’, 'cityboss.webp’,
+    'darkcandle.webp’,'hospital.webp’, 'mountain.webp’,'sanrin.webp’, 'yakerin.webp’,
+    'shiina.webp’, 'urashiina.webp’,'baku01.webp’,'igari_jiki.webp’,'igaribomb.webp’,
     // --- ステージ3追加分 ---
-    'shiinaboss.png','shiki.png','sans.png',
+    'shiinaboss.webp’,'shiki.webp’,'sans.webp’,
     // --- 新規追加分 ---
-    'advsnow.png','jingu.png','robot.png','boro.png',
-    'rei.png','renji.png','snow.png','sui.png','mask.png',
-    'tv.png','advrocket.png','aboro.png','robodon.png',
-    'potable.png','potable2.png','phone.png','bluesky.png','phone2.png','phone3.png',
+    'advsnow.webp’,'jingu.webp’,'robot.webp’,'boro.webp’,
+    'rei.webp’,'renji.webp’,'snow.webp’,'sui.webp’,'mask.webp’,
+    'tv.webp’,'advrocket.webp’,'aboro.webp’,'robodon.webp’,
+    'potable.webp’,'potable2.webp’,'phone.webp’,'bluesky.webp’,'phone2.webp’,'phone3.webp’,
     // --- 新規追加分 ---
-    'moon.jpg','godai.png','univ.png','postuniv.png','godaimo.png','gtypea.png','gtypeb.png','gtypec.png',
-    'gtypeboss.png','cap.png',
+    'moon.jpg','godai.webp’,'univ.webp’,'postuniv.webp’,'godaimo.webp’,'gtypea.webp’,'gtypeb.webp’,'gtypec.webp’,
+    'gtypeboss.webp’,'cap.webp’,
     // --- 新規追加分 ---
-    'trench_floor.png','trench_wall.png','​core_bg.png','core_reactor.png','gtyped.png','gtypee.png'
-    ,'cap01.png','uramoon.png','base.png','inbase.png','brokenbase.png', 'airport02.png', 'jikishi.png'
-    ,'jikishi.png','chain.png','ope.png','kagejingu.png','nurse.png','shiinake.png','eiji.png','tadashige.png'
-    ,'mask2.png','uraeiji.png','playereiji.png','nightmt.png','nightmtstg.png','kagekagami.png'
+    'trench_floor.webp’,'trench_wall.webp’,'​core_bg.webp’,'core_reactor.webp’,'gtyped.webp’,'gtypee.webp’
+    ,'cap01.webp’,'uramoon.webp’,'base.webp’,'inbase.webp’,'brokenbase.webp’, 'airport02.webp’, 'jikishi.webp’
+    ,'jikishi.webp’,'chain.webp’,'ope.webp’,'kagejingu.webp’,'nurse.webp’,'shiinake.webp’,'eiji.webp’,'tadashige.webp’
+    ,'mask2.webp’,'uraeiji.webp’,'playereiji.webp’,'nightmt.webp’,'nightmtstg.webp’,'kagekagami.webp’
 ];
-
+ 
 export const imagesToPreload3D = [
-    { key: 'sideatlas', src: 'build_side.png' }, 
-    { key: 'topatlas', src: 'build_top.png' },
-    { key: 'ground', src: 'ground01.png' },
-    { key: 'ground2', src: 'ground02.png' }, 
-    { key: 'candle', src: 'candle.png' },
+    { key: 'sideatlas', src: 'build_side.webp’ },
+    { key: 'topatlas', src: 'build_top.webp’ },
+    { key: 'ground', src: 'ground01.webp’ },
+    { key: 'ground2', src: 'ground02.webp’ },
+    { key: 'candle', src: 'candle.webp’ },
     { key: 'moon', src: 'moon.jpg' } ,
     // ★最終ステージ用 追加分
-    { key: 'trenchFloor', src: 'trench_floor.png' },
-    { key: 'trenchWall', src: 'trench_wall.png' },
-    { key: 'coreBg', src: 'core_bg.png' },
-    { key: 'coreReactor', src: 'core_reactor.png' }
+    { key: 'trenchFloor', src: 'trench_floor.webp’ },
+    { key: 'trenchWall', src: 'trench_wall.webp’ },
+    { key: 'coreBg', src: 'core_bg.webp’ },
+    { key: 'coreReactor', src: 'core_reactor.webp’ }
 ];
